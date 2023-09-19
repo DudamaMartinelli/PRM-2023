fb29779a58db3bbaf0e4e7e3136bfab3114e334d
Maria Eduarda Martinelli <95435982+DudamaMartinelli@users.noreply.github.com>
2023-09-19 13:48:42 -0300

    Aula 19/09/2023 alterações e aplicação com o Mysql.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,25 @@ import { AppService } from './app.service';
 
 import { ProfileService } from './services/profile.service';
 import { ProfileController } from './controllers/profile.controller';
+import {TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './controllers/user.controller';
+import { UserService } from './services/user.service';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProfileController],
-  providers: [AppService, ProfileService],
+  imports: [
+     TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'prm_2023',
+      entities: [User]
+    }),
+    TypeOrmModule.forFeature([User])
+  ],
+  controllers: [AppController, ProfileController, UserController],
+  providers: [AppService, ProfileService, UserService],
 })
 export class AppModule {}
